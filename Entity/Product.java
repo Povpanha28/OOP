a15bad579272bedc77fc8c05584877a13826e598
Povpanha28 <sengkim28140@gmail.com>
2025-01-22 14:36:21 +0700

    add enitty

---
+++ Entity/Product.java
@@ -0,0 +1,13 @@
+package Entity;
+// Purpose: Manages the details of items available for sale
+
+public class Product {
+
+    String name;
+    int id;
+    double price;
+    int qty;
+    String description;
+    boolean status;
+    
+}
---
+++ Entity/Sale.java
@@ -0,0 +1,8 @@
+package Entity;
+public class Sale {
+
+    int saleID;
+    int amount;
+    
+
+}
