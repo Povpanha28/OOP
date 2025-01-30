c46174d44582e159536e329313c495ea53bff1d4
Povpanha28 <sengkim28140@gmail.com>
2025-01-30 10:14:24 +0700

    delete getter setter

--- Entity/Product.java
+++ Entity/Product.java
@@ -4,14 +4,14 @@ package Entity;
 
 public class Product {
 
-    private String name;
-    private int id;
-    private double price;
-    private int qty;
-    private String description;
-    private boolean status;
-    private String addedDate;
-    private String expiredDate;
+    String name;
+    int id;
+    double price;
+    int qty;
+    String description;
+    boolean status;
+    String addedDate;
+    String expiredDate;
 
     // Constructor
     Product(String name, int id, String addedDate, int qty, String expiredDate) {
@@ -19,72 +19,6 @@ public class Product {
         this.id = id;
         this.addedDate = addedDate;
         this.qty = qty;
-        this.expiredDate = expiredDate;
-    }
-
-    // Getters
-    public String getName() {
-        return name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public int getQty() {
-        return qty;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public boolean getStatus() {
-        return status;
-    }
-
-    public String getAddedDate() {
-        return addedDate;
-    }
-
-    public String getExpiredDate() {
-        return expiredDate;
-    }
-
-    // Setters
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public void setQty(int qty) {
-        this.qty = qty;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-    public void setAddedDate(String addedDate) {
-        this.addedDate = addedDate;
-    }
-
-    public void setExpiredDate(String expiredDate) {
         this.expiredDate = expiredDate;
     }
 }
--- Entity/Sale.java
+++ Entity/Sale.java
@@ -1,12 +1,12 @@
 package Entity;
 
 public class Sale {
-    private String name;
-    private int saleID;
-    private int amount;
-    private int customerID;
-    private String saleDate;
-    private String paymentMethod;
+    String name;
+    int saleID;
+    int amount;
+    int customerID;
+    String saleDate;
+    String paymentMethod;
 
     // Constructor
     public Sale(int saleID, int amount, int customerID, String paymentMethod) {
@@ -14,55 +14,5 @@ public class Sale {
         this.amount = amount;
         this.customerID = customerID;
         this.paymentMethod = paymentMethod;
-    }
-
-    // Getters
-    public int getSaleID() {
-        return saleID;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public int getCustomerID() {
-        return customerID;
-    }
-
-    public String getPaymentMethod() {
-        return paymentMethod;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSaleDate() {
-        return saleDate;
-    }
-
-    // Setters
-    public void setSaleID(int saleID) {
-        this.saleID = saleID;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public void setCustomerID(int customerID) {
-        this.customerID = customerID;
-    }
-
-    public void setPaymentMethod(String paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSaleDate(String saleDate) {
-        this.saleDate = saleDate;
     }
 }
