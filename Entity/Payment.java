9b3c1b7d62c85def02d91995e08c9108804c72a7
Thyrak12 <thyrak9167@gmail.com>
2025-02-05 14:30:28 +0700

    set security for payment

--- Entity/Payment.java
+++ Entity/Payment.java
@@ -7,14 +7,13 @@ public class Payment {
     private String paymentMethod;
     private String transactionDate;
     private double amountPaid;
-
+    private final String adminPassword = "SecurePass123";
 
     // Constructor (Public: Allows object creation from anywhere)
-    public Payment(int paymentID, int saleID, String paymentMethod, String transactionDate, double amountPaid) {
+    public Payment(int paymentID, int saleID, String paymentMethod, double amountPaid) {
         paymentID++;
         this.saleID = saleID;
         this.paymentMethod = paymentMethod;
-        this.transactionDate = transactionDate;
         this.amountPaid = amountPaid;
     }
 
@@ -39,26 +38,40 @@ public class Payment {
         return amountPaid;
     }
 
-
-    public void setSaleID(int saleID) {
-        this.saleID = saleID;
+    // Password
+    private boolean isAuthorized(String password) {
+        return password.equals(adminPassword);
     }
 
-    public void setPaymentMethod(String paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
+    // Setter methods
 
-    public void setTransactionDate(String transactionDate) {
-        this.transactionDate = transactionDate;
-    }
-
-    public void setAmountPaid(double amountPaid) {
-        if (amountPaid >= 0) {
-            this.amountPaid = amountPaid;
+    public void setSaleID(int saleID, String password) {
+        if (isAuthorized(password)) {
+            this.saleID = saleID;
         } else {
-            System.out.println("Invalid amount. Payment amount cannot be negative.");
+            System.out.println("Unauthorized access: Invalid password.");
         }
     }
 
-}
+    public void setPaymentMethod(String paymentMethod, String password) {
+        if (isAuthorized(password)) {
+            this.paymentMethod = paymentMethod;
+        } else {
+            System.out.println("Unauthorized access: Invalid password.");
+        }
+    }
 
+    public void setAmountPaid(double amountPaid, String password) {
+        if (isAuthorized(password)) {
+            if (amountPaid >= 0) {
+                this.amountPaid = amountPaid;
+            } else {
+                System.out.println("Invalid amount. Payment amount cannot be negative.");
+            }
+        } else {
+            System.out.println("Unauthorized access: Invalid password.");
+        }
+
+    }
+
+}
