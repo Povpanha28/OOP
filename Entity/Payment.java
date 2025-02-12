608e18dfd9497e06cb23bf92047ec3609b47f7ea
Thyrak12 <thyrak9167@gmail.com>
2025-02-12 20:45:00 +0700

    add hashmap to payment

--- Entity/Payment.java
+++ Entity/Payment.java
@@ -1,49 +1,69 @@
 package Entity;
 
+import java.util.HashMap;
 
 public class Payment {
 
-    private static int paymentID = 0;
-    private int saleID; 
+    private int paymentID;
+    private static int nextPaymentID = 1;
+    private int saleID;
     private String paymentMethod;
     private String transactionDate;
     private double amountPaid;
 
     private final String adminPassword = "SecurePass123";
+    private static HashMap<Integer, Payment> paymentDatabase = new HashMap<>();
 
     // Constructor (Public: Allows object creation from anywhere)
-    public Payment(int saleID,  double amountPaid, String transactionDate, String paymentMethod) {
-        paymentID++;
+    public Payment(int saleID, double amountPaid, String transactionDate, String paymentMethod) {
+        this.paymentID = nextPaymentID;
+        nextPaymentID++;
         this.saleID = saleID;
         this.paymentMethod = paymentMethod;
         this.amountPaid = amountPaid;
         this.transactionDate = transactionDate;
+        paymentDatabase.put(this.paymentID, this);
     }
 
     // Getter methods (Public: Provides controlled access to private variables)
-    public int getPaymentID() {
-        return paymentID;
+    public int getPaymentID() {return paymentID;}
+    public int getSaleID() {return saleID;}
+    public String getPaymentMethod() {return paymentMethod;}
+    public String getTransactionDate() {return transactionDate;}
+    public double getAmountPaid() {return amountPaid;}
+
+    public static Payment getPaymentByID(int id) {
+        return paymentDatabase.get(id); // Retrieve payment by ID
     }
 
-    public int getSaleID() {
-        return saleID;
+    public static void removePaymentByID(int id, String password) {
+        Payment payment = paymentDatabase.get(id);
+        if (payment != null && payment.isAuthorized(password)) {
+            paymentDatabase.remove(id);
+            System.out.println("Payment with ID " + id + " removed successfully.");
+        } else {
+            System.out.println("Unauthorized access or payment not found.");
+        }
     }
 
-    public String getPaymentMethod() {
-        return paymentMethod;
-    }
-
-    public String getTransactionDate() {
-        return transactionDate;
-    }
-
-    public double getAmountPaid() {
-        return amountPaid;
+    public static HashMap<Integer, Payment> getAllPayments() {
+        return paymentDatabase; // Retrieve all payments
     }
 
     // Password
     private boolean isAuthorized(String password) {
         return password.equals(adminPassword);
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "PaymentID=" + paymentID +
+                ", SaleID=" + saleID +
+                ", PaymentMethod='" + paymentMethod + '\'' +
+                ", TransactionDate='" + transactionDate + '\'' +
+                ", AmountPaid=" + amountPaid +
+                '}';
     }
 
     // Setter methods
