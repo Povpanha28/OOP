e356a7e318c404aae38cab6cfdf21fb938bd1609
youdyy17 <105262374+youdyy17@users.noreply.github.com>
2025-02-12 23:59:23 +0700

    Update test_customer.java

--- test_file/test_customer.java
+++ test_file/test_customer.java
@@ -1,14 +1,19 @@
 package test_file;
+
 import Entity.Customer;
 
 public class test_customer {
     public static void main(String[] args) {
-        Customer.addCustomer(1, "John Doe", "1234567890", "123 Main St");
-        Customer.addCustomer(2, "Jane Dy", "0987654321", "456 Elm St");
-        System.out.println("Total customers: " + Customer.totalCustomer());
-        System.out.println("Customer 1: " + Customer.findCustomer(1).getCustomerName());
-        System.out.println("Customer 2: " + Customer.findCustomer(2).getCustomerName());
-        Customer.removeCustomer(1);
-        System.out.println("Total customers: " + Customer.totalCustomer());
+        // Add customer
+        Customer.addCustomer(1, "John Doe", "0123456789", "123 Main St", 1234, "johndoe");
+        Customer.addCustomer(2, "Jane Smith", "0987654321", "456 Elm St", 5678, "janesmith");
+
+        // Display total customers
+        System.out.println("There are: " + Customer.totalCustomer() + " customers.");
+
+        // Display all customers
+        Customer.displayAllCustomers();
+        // test login
+        Customer.authenticateCustomer("johndoe", 1274);
     }
 }
