8abc98f56c971cf604ac2b18334c5c24dfce24a4
Povpanha28 <sengkim28140@gmail.com>
2025-02-19 21:05:14 +0700

    k

--- Entity/Test.java
+++ Entity/Test.java
@@ -4,8 +4,13 @@ package Entity;
 public class Test {
     @SuppressWarnings("unused")
     public static void main(String[] args) {
-        Product product1 = new Product("Apple",20,"02-04-2024","02-04-2027","S001");
-        Product product2 = new Product("Banana",30,"02-04-2024","02-04-2027","S002");
-        System.out.println(Product.getProductById(2));
+        // Product product1 = new Product("Apple",20,"02-04-2024","02-04-2027","S001");
+        // Product product2 = new Product("Banana",30,"02-04-2024","02-04-2027","S002");
+        // System.out.println(Product.getProductById(2));
+
+        Sale sale1 = new Sale(1,1,2);
+        Sale sale2 = new Sale(2,2,3);
+        System.out.println(Sale.getAllSales());
+        
     }
 }
